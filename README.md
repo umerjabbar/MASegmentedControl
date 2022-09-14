b22904686a80aad4d53c942cd1c0498852343963
Umer Jabbar <umerabduljabbar@icloud.com>
2022-09-14 22:28:10 +0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MASegmentedControl
+
+A description of this package.
---
+++ Sources/MASegmentedControl/MASegmentedControl.swift
@@ -0,0 +1,6 @@
+public struct MASegmentedControl {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
